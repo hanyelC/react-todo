56f2f394c27d21b968b380f08ecbbd3da887a1fb
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-02-06 11:39:37 -0300

    feat: create DeleteButtom component

--- src/components/DeleteButton/index.tsx
+++ src/components/DeleteButton/index.tsx
@@ -1,1 +1,10 @@
 import styles from './styles.module.css'
+import { Trash } from '../../assets/trash'
+
+export function DeleteButton() {
+  return (
+    <button className={styles['delete-button']}>
+      <Trash />
+    </button>
+  )
+}
