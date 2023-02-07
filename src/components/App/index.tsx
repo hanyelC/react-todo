799c71e725e0c72c138f9c50e0070e1f8ade436a
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-02-07 09:22:15 -0300

    feat: implement the functionality

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { CreateButton } from '../CreateButton';
 import { Header } from '../Header';
 import { Input } from '../Input';
@@ -5,33 +6,95 @@ import { Task } from '../Task';
 import { TasksPlaceholder } from '../TasksPlaceholder';
 import styles from './styles.module.css';
 
+type TaskData = {
+  id: string;
+  done: boolean;
+  text: string;
+};
+
 export function App() {
+  const [tasks, setTasks] = useState<TaskData[]>([]);
+  const [newTaskValue, setNewTaskValue] = useState<string>('');
+
+  const totalOfTasks = tasks.length;
+
+  const totalOfDoneTasks = tasks.reduce((accumulator, { done }) => {
+    if (done) accumulator++;
+    return accumulator;
+  }, 0);
+
+  const handleCreateTask = () => {
+    if (newTaskValue.length === 0) return;
+
+    const id = `${new Date().toISOString()} ${newTaskValue}`;
+
+    const newTask: TaskData = {
+      done: false,
+      id,
+      text: newTaskValue
+    };
+
+    setTasks((prevState) => [...prevState, newTask]);
+
+    setNewTaskValue('');
+  };
+
+  const handleRemoveTask = (taskId: string) => {
+    setTasks((prevState) => prevState.filter((task) => task.id !== taskId));
+  };
+
+  const handleToggleTask = (taskId: string) => {
+    const updatedTasks = tasks.map((task) => {
+      if (taskId === task.id) task.done = !task.done;
+
+      return task;
+    });
+
+    setTasks(updatedTasks);
+  };
+
   return (
     <div className={styles.App}>
       <Header />
 
       <main className={styles.main}>
         <div className={styles['new-task']}>
-          <Input /> <CreateButton />
+          <Input
+            onChange={(e) => setNewTaskValue(e.target.value)}
+            value={newTaskValue}
+          />
+          <CreateButton onClick={handleCreateTask} />
         </div>
         <div className={styles['tasks-container']}>
           <div className={styles['tasks-header']}>
             <div>
               <span>Tarefas criadas</span>
-              <span className={styles.counter}>0</span>
+              <span className={styles.counter}>{totalOfTasks}</span>
             </div>
             <div>
               <span>Concluídas</span>
-              <span className={styles.counter}>0 de 0</span>
+              <span
+                className={styles.counter}
+              >{`${totalOfDoneTasks} de ${totalOfTasks}`}</span>
             </div>
           </div>
-          {/* <TasksPlaceholder /> */}
-          <div className={styles['tasks-list']}>
-            <Task done />
-            <Task />
-            <Task done />
-            <Task />
-          </div>
+          {tasks.length > 0 ? (
+            tasks.map((task) => (
+              <Task
+                key={task.id}
+                done={task.done}
+                onRemoveTask={() => {
+                  handleRemoveTask(task.id);
+                }}
+                onToggleTask={() => {
+                  handleToggleTask(task.id);
+                }}
+                text={task.text}
+              />
+            ))
+          ) : (
+            <TasksPlaceholder />
+          )}
         </div>
       </main>
     </div>
