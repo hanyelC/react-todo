f6f39c6d52cff5b65111efaa8f6e483c700ff1eb
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-02-07 09:18:34 -0300

    feat: accept props on CreateButton component

--- src/components/CreateButton/index.tsx
+++ src/components/CreateButton/index.tsx
@@ -1,11 +1,14 @@
-import styles from './styles.module.css'
-import plus from '../../assets/plus.svg'
+import { ButtonHTMLAttributes } from 'react';
+import plus from '../../assets/plus.svg';
+import styles from './styles.module.css';
 
-export function CreateButton () {
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {}
+
+export function CreateButton({ ...props }: Props) {
   return (
-    <button className={styles.button}>
+    <button className={styles.button} {...props}>
       Criar
       <img src={plus} alt="plus" />
     </button>
-  )
+  );
 }
