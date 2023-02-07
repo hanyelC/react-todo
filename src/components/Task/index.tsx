033eb207f3e85fd50059cdd6ed10dcfccc676a76
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-02-07 08:35:43 -0300

    feat: accept props on Task component

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -4,14 +4,22 @@ import styles from './styles.module.css';
 
 interface Props {
   done?: boolean;
+  onRemoveTask: () => void;
+  onToggleTask: () => void;
+  text: string;
 }
 
-export function Task({ done = false }: Props) {
+export function Task({
+  done = false,
+  onRemoveTask,
+  onToggleTask,
+  text
+}: Props) {
   return (
     <div className={`${styles.task} ${done ? styles['task-done'] : ''}`}>
-      <Checkbox />
-      <span>Task</span>
-      <DeleteButton />
+      <Checkbox checked={done} onChange={onToggleTask} />
+      <span>{text}</span>
+      <DeleteButton onClick={onRemoveTask}  />
     </div>
   );
 }
